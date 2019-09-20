da76655a573ee1582bd8535c724390b73c9df980
mrtsven <zahharov@hotmail.com>
2019-09-20 15:33:02 +0200

    Activate limb (dummy)

--- Assets/Scripts/CollectibleScript.cs
+++ Assets/Scripts/CollectibleScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CollectibleScript : MonoBehaviour
 {
     public GameObject limb;
-    
+
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "Player"){
             transform.gameObject.SetActive(false);
+            limb.SetActive(true);
         }
     }
 }
