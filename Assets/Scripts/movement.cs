ced0ad7a70f6140726bad96e695159673fe38c2c
Pasi Saarikalle <pasi.saarikalle@gmail.com>
2019-09-20 15:53:53 +0300

    Movement script
    Point and click movement script

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+    public UnityEngine.Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
